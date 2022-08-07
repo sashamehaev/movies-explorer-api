88c556448f93a8892a53fd07b89d99bb5c4c2a8e
Alexandr Mekhaev <alexandrmekhaev@yandex.ru>
2022-08-08 02:25:07 +0300

    userSchema

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/moviesdb');
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
 
 app.listen(PORT, () => {
-
+  console.log('listening');
 });
